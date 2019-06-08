dc1e9de05e87713a736fe12887e87bae70d16efb
elickzhao <xwiwi@foxmail.com>
2019-06-08 16:04:27 +0800

    加了兼容 swiper 可以从04和small图片同时可用

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,13 @@
-export const appId = 'wx89eb02efdb4ddaaf';
+export const appId = 'wx9694b8aea233649e';
 
-export const hostUrl = 'https://small.huanqiujishi.com';
+export const hostUrl = 'https://hqjs0004.huanqiujishi.com/';
 
 export const baseUrl = hostUrl + '/index.php';
 
-export const imgUrl = hostUrl + '/Data/';
+// export const imgUrl = hostUrl + 'Data/';
+
+export const imgUrl = 'https://small.huanqiujishi.com/Data/';
 
 export const minimum = '68';
 
-export const appName = 'TFS+';
+export const appName = '代购仓';
