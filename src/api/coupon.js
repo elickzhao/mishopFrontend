75a0687dcbf6895ced6ffaf66a5b1f1080a264cb
elickzhao <xwiwi@foxmail.com>
2019-06-16 01:14:46 +0800

    领券增加积分限制

--- src/api/coupon.js
+++ src/api/coupon.js
@@ -2,28 +2,27 @@
  * @Author: elick
  * @LastEditors: elick
  * @Date: 2019-02-28 01:44:02
- * @LastEditTime: 2019-03-11 01:16:07
+ * @LastEditTime: 2019-06-15 22:20:05
  * @Description: 优惠券api
  */
 import base from './base';
-import Page from '../utils/Page';
+// import Page from '../utils/Page';
 
 export default class coupon extends base {
   /**
    * 获取各个类型订单数量
-   * @param {*} params 
+   * @param {*} params
    */
-  static getOrderInfo(params) {
+  static getOrderInfo (params) {
     let url = `${this.baseUrl}/api/news/getOrderCountNew`;
     return this.get(url, params)
   }
 
-
   /**
    * 会员中心领取优惠券页面 优惠券列表
-   * @param {*} params 
+   * @param {*} params
    */
-  static getCouponList(params) {
+  static getCouponList (params) {
     let url = `${this.baseUrl}/api/news/getCouponListNew`;
     return this.get(url, params).then(data => {
       return data
@@ -34,9 +33,9 @@ export default class coupon extends base {
 
   /**
    * 操作订单状态
-   * @param {*} params 
+   * @param {*} params
    */
-  static getCustCouponList(params) {
+  static getCustCouponList (params) {
     let url = `${this.baseUrl}/api/news/getCustCouponListNew`;
     return this.get(url, params).then(data => {
       return data
@@ -47,9 +46,9 @@ export default class coupon extends base {
 
   /**
    * 获取优惠券
-   * @param {*} params 
+   * @param {*} params
    */
-  static addVoucher(params) {
+  static addVoucher (params) {
     let url = `${this.baseUrl}/api/news/addVoucherNew`;
     return this.get(url, params).then(data => {
       return data
@@ -57,8 +56,4 @@ export default class coupon extends base {
       return error
     })
   }
-
-  
-
-
 }
